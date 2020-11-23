12ed8df6c28ddfedbc9cafa0aee49492f1d470cb
Nguyễn Văn Đạt - CMC Global DU1 <nvdat2@ldap.cmcglobal.com.vn>
2020-11-23 10:53:11 +0700

    finish write log

--- src/main/java/com/example/demoswagger/DemoSwaggerApplication.java
+++ src/main/java/com/example/demoswagger/DemoSwaggerApplication.java
@@ -2,8 +2,13 @@ package com.example.demoswagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.jmx.support.RegistrationPolicy;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+//@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
+//@EnableTransactionManagement
 public class DemoSwaggerApplication {
 
     public static void main(String[] args) {
---
+++ src/main/resources/init.sql
@@ -0,0 +1,5 @@
+create database swagger_log;
+
+use swagger_log;
+
+create table students(id int auto_increment primary key , name varchar(250), cls varchar(250), country varchar(250));
